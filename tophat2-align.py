d71dda67695339021057016d23a88812986662af
Kaur Alasoo <kaur.alasoo@gmail.com>
2013-09-18 18:31:42 +0100

    tophat2

--- tophat2-align.py
+++ tophat2-align.py
@@ -7,13 +7,14 @@ import argparse
 parser = argparse.ArgumentParser(description = "Align RNA-Seq reads to the reference genome using TopHat2.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("reads", nargs = "*", help = "Path to FASTQ files.")
 parser.add_argument("--gtf", help = "GTF/GFF file for TopHat", 
-	default = "/nfs/teams/team170/Ensembl/Homo_sapiens.GRCh37.69.gtf")
+	default = "/nfs/users/nfs_k/ka8/group-scratch/kaur/annotations/GRCh37/Ensembl_72/Homo_sapiens.GRCh37.72.gff")
 parser.add_argument("--index", help = "Bowtie2 index location.", 
 	default = "/nfs/users/nfs_k/ka8/group-scratch/kaur/annotations/GRCh37/bowtie2-index/GRCh37")
 parser.add_argument("--MEM", help = "Memory requirements for farm (MB).", default = "4500")
 parser.add_argument("--out", help = "TopHat2 output folder.", default = "./tophat_out")
 parser.add_argument("--ncores", help = "Number of cores to use.", default = "1")
 parser.add_argument("--txindex", help = "Path to transcriptome index")
+parser.add_argument("--library", help = "library type for TopHat2.")
 args = parser.parse_args()
 
 #Set up TopHat arguments
@@ -25,6 +26,8 @@ tophat2_arguments = ["tophat2",
 #Add path to transcriptome index
 if args.txindex:
 	tophat2_arguments = tophat2_arguments + ["--transcriptome-index " + args.txindex]
+if args.library:
+	tophat2_arguments = tophat2_arguments + ["--library-type " + args.library]
 
 tophat2_command = " ".join(tophat2_arguments + [args.index] + args.reads)
 memory_string = "".join(['-R"select[mem>',args.MEM,'] rusage[mem=', args.MEM, ']" -M', args.MEM, " -G team170"])
