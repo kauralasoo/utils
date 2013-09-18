080f84134c6acd46f687f75a9aa4cb43415dcb6c
Kaur Alasoo <kaur.alasoo@gmail.com>
2013-09-18 23:54:41 +0100

    alignment scripts

--- tophat2-align.py
+++ tophat2-align.py
@@ -13,7 +13,8 @@ parser.add_argument("--index", help = "Bowtie2 index location.",
 parser.add_argument("--MEM", help = "Memory requirements for farm (MB).", default = "4500")
 parser.add_argument("--out", help = "TopHat2 output folder.", default = "./tophat_out")
 parser.add_argument("--ncores", help = "Number of cores to use.", default = "1")
-parser.add_argument("--txindex", help = "Path to transcriptome index")
+parser.add_argument("--txindex", help = "Path to transcriptome index", 
+	default = "/nfs/users/nfs_k/ka8/group-scratch/kaur/annotations/GRCh37/Ensembl_72/Homo_sapiens.GRCh37.72")
 parser.add_argument("--library", help = "library type for TopHat2.")
 args = parser.parse_args()
 
---
+++ tophat2-wrapper.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python2.7
+import os
+import sys
+import argparse
+import fileinput
+
+parser = argparse.ArgumentParser(description = "Submit multiple Tophat2 alignment jobs to Farm3", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--MEM", help = "Memory requirements for farm (MB).", default = "4500")
+parser.add_argument("--out", help = "TopHat2 output folder.", default = "tophat2")
+parser.add_argument("--fastq", help = "Path to folder containing FASTQ files", default = "fastq")
+parser.add_argument("--ncores", help = "Number of cores to use.", default = "1")
+args = parser.parse_args()
+
+print args.MEM
+print args.out
+
+for line in fileinput.input("-"):
+	line = line.rstrip()
+	script_path = "/nfs/users/nfs_k/ka8/software/utils/tophat2-align.py"
+	output_dir = os.path.join(args.out, line)
+
+	#Fastq files
+	fq1 = os.path.join(args.fastq, line + ".1.fq.gz")
+	fq2 = os.path.join(args.fastq, line + ".2.fq.gz")
+	
+	#Create output dir if it does not exist
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
+	command = " ".join([script_path, "--MEM", args.MEM, "--out", output_dir, "--ncores", args.ncores, fq1, fq2 ])
+	print(command)
+	os.system(command)
