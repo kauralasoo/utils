b94a15f0498568f158e52c602e053bee466a8c74
Kaur Alasoo <kaur.alasoo@gmail.com>
2013-10-13 17:42:02 +0100

    bedgraph conversion

--- bam2bedgraph.py
+++ bam2bedgraph.py
@@ -10,6 +10,8 @@ parser.add_argument("--bigwig", help = "Path to BigWig files", default = "BigWig
 parser.add_argument("--chrlengths", help = "Path to gene annotations in GTF format", 
 	default = "/nfs/users/nfs_k/ka8/group-scratch/kaur/annotations/GRCh37/bowtie2-index/chrom-sizes.txt")
 parser.add_argument("--execute", help = "If True then executes the command, otherwise just prints it out.", default  = "True")
+parser.add_argument("--split", help = "Treat the alignments as split or not.", default  = "True")
+
 args = parser.parse_args()
 
 #Iterate over all ids
@@ -18,7 +20,11 @@ for line in fileinput.input("-"):
 	bam_file = os.path.join(args.bams, line + ".bam")
 	bg_file = os.path.join(args.bedgraphs, line + ".bg")
 	bw_file = os.path.join(args.bigwig, line + ".bw")
-	command = " ".join(["bedtools genomecov -bga -split -ibam", bam_file, "-g", args.chrlengths, ">", bg_file])
+	if args.split == "True":
+		command = " ".join(["bedtools genomecov -bga -ibam -split", bam_file, "-g", args.chrlengths, ">", bg_file])
+	else:
+		command = " ".join(["bedtools genomecov -bga -ibam", bam_file, "-g", args.chrlengths, ">", bg_file])
+
 	bw_command = " ".join(["bedGraphToBigWig", bg_file, args.chrlengths, bw_file])
 	print(command)
 	print(bw_command)
