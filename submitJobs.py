d5d3204e22c6eb2d9d1635f18342358cd45ec079
Kaur Alasoo <kaur.alasoo@gmail.com>
2014-08-26 14:40:54 +0100

    Specify queue in submitJobs

--- submitJobs.py
+++ submitJobs.py
@@ -9,6 +9,7 @@ parser.add_argument("--command", help = "Excact command to be submitted.")
 parser.add_argument("--MEM", help = "Amount of memory required.")
 parser.add_argument("--farmout", help = "Folder for FARM log files", default = "FarmOut")
 parser.add_argument("--ncores", help = "Number of cores to use", default = "1")
+parser.add_argument("--queue", help = "Queue for submitting the jobs into.", default = "normal")
 args = parser.parse_args()
 
 memory_string = ' -R"span[hosts=1] select[mem>' + args.MEM +'] rusage[mem=' + args.MEM +']" -M '+args.MEM
@@ -16,8 +17,7 @@ output_file = os.path.join(args.farmout, args.jobname + '.%J.txt')
 
 for line in fileinput.input("-"):
 	line = line.rstrip()
-	command = "".join(["bsub -G team170 -o ", output_file, " -n " + args.ncores, memory_string, " -J ", args.jobname, " \"echo '", line,"'", " | ", args.command, "\""])
+	command = "".join(["bsub -G team170 -o ", output_file, " -q " + args.queue, " -n " + args.ncores, memory_string, " -J ", args.jobname, " \"echo '", line,"'", " | ", args.command, "\""])
 	print(command) 
 	os.system(command)
-
 
