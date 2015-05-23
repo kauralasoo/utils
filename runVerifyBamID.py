e9b21bb3404b2c2532b71c20017084b5671fe388
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-05-23 22:32:50 +0100

    Check concordance between VCFs and BAMS

--- runVerifyBamID.py
+++ runVerifyBamID.py
@@ -4,17 +4,19 @@ import argparse
 import fileinput
 import subprocess
 
-parser = argparse.ArgumentParser(description = "Index bam files using samtools.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description = "Test concordance between BAM and VCF files using verifyBamID.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--bamdir", help = "Directory of the BAM files.")
 parser.add_argument("--insuffix", help = "Suffix of the input bam file.", default = ".bam")
+parser.add_argument("--vcf", help = "Path to the VCF file.")
 parser.add_argument("--execute", help = "Execute the script", default = "False")
 args = parser.parse_args()
 
 #Construct file names
 for line in fileinput.input("-"):
 		sample_name = line.rstrip()
-		path_in = os.path.join(args.bamdir, sample_name, sample_name + args.insuffix)
-		command = " ".join(["samtools index", path_in])
+		bam_path = os.path.join(args.bamdir, sample_name, sample_name + args.insuffix)
+		out_prefix = os.path.join(args.bamdir, sample_name, sample_name + ".verifyBamID")
+		command = " ".join(["verifyBamID.1.1.2 --vcf", args.vcf, "--bam", bam_path, "--out", out_prefix, "--best"])
 		print(command)
 		if args.execute == "True":
 			subprocess.call(['bash','-c',command])
