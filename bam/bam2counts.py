062ab1cdb2be7a29a0034442ebb322d76ce77f4e
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-09-02 00:13:58 +0100

    Add support to count reads from SE BAMs

--- bam/bam2counts.py
+++ bam/bam2counts.py
@@ -12,6 +12,8 @@ parser.add_argument("--gtf", help = "Path to gene annotations in GTF format")
 parser.add_argument("--execute", help = "If True then executes the command, otherwise just prints it out.", default  = "True")
 parser.add_argument("--strand", help = "0 (unstranded); 1 (stranded); 2(reversely stranded)", default = "0")
 parser.add_argument("--multimapping", help = "Count multimapping reads.", default = "False")
+parser.add_argument("--unpaired", help = "BAM contains single-end reads.", default = "False")
+
 args = parser.parse_args()
 
 #Iterate over all ids
@@ -19,9 +21,11 @@ for line in fileinput.input("-"):
 	line = line.rstrip()
 	bam_file = os.path.join(args.sampleDir, line, line + args.bamSuffix)
 	count_file = os.path.join(args.sampleDir, line, line + args.countsSuffix)
-	featureCounts_command = " ".join(["featureCounts -a", args.gtf, "-o", count_file, "-p -C -D 2000 -d 25", "-s", args.strand])
+	featureCounts_command = " ".join(["featureCounts -a", args.gtf, "-o", count_file, "-s", args.strand])
 	if(args.multimapping == "True"):
 		featureCounts_command = featureCounts_command + " -M"
+	if(args.unpaired == "False"):
+		featureCounts_command = featureCounts_command + "-p -C -D 2000 -d 25"
 	command = " ".join([featureCounts_command, bam_file])
 	print(command)
 	if (args.execute == "True"):
