a3c926c2642767628a33833f764ec55d02ba9067
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-09-11 09:49:27 +0300

    fix path to script

--- bam/bamToFragmentBed.py
+++ bam/bamToFragmentBed.py
@@ -17,7 +17,7 @@ for line in fileinput.input("-"):
 		sample_name = line.rstrip()
 		path_in = os.path.join(args.indir, sample_name, sample_name + args.insuffix)
 		path_out = os.path.join(args.outdir, sample_name, sample_name + args.outsuffix)
-		bedpe_to_bed = "python ~/software/utils/bedpe2bed.py --maxFragmentLength 1000 | sort -k 1,1 | gzip"
+		bedpe_to_bed = "python ~/software/utils/coverage/bedpe2bed.py --maxFragmentLength 1000 | sort -k 1,1 | gzip"
 		command = " ".join(["bedtools bamtobed -bedpe -i", path_in , "|", bedpe_to_bed, ">", path_out])
 		print(command)
 		if args.execute == "True":
