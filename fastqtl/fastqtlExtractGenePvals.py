62e13e61adb75ef9bd0220ca5ef06f8a144bd7f0
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-12-11 14:57:38 +0200

    Extract gene pvalues

---
+++ fastqtl/fastqtlExtractGenePvals.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import argparse
+import fileinput
+import subprocess
+import gzip
+
+parser = argparse.ArgumentParser(description = "Filter fastqtl output file by maximum p-value.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--fastqtl", help = "Path to the fastqtl output file with snp coordinates.")
+parser.add_argument("--gene_id", help = "ID of the gene to be extracted from the file.", type = float)
+args = parser.parse_args()
+
+print "CHR BP SNP P"
+command = " ".join(["zgrep", args.gene_id, args.fastqtl '| cut -f2,3,4,6 -d " "'])
+subprocess.call(['bash','-c',command])
