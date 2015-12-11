6b3bc96e4d98f15df231def5db62d511012be342
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-12-11 14:25:41 +0000

    print header correctly

--- fastqtl/fastqtlExtractGenePvals.py
+++ fastqtl/fastqtlExtractGenePvals.py
@@ -7,9 +7,12 @@ import gzip
 
 parser = argparse.ArgumentParser(description = "Filter fastqtl output file by maximum p-value.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--fastqtl", help = "Path to the fastqtl output file with snp coordinates.")
-parser.add_argument("--gene_id", help = "ID of the gene to be extracted from the file.", type = float)
+parser.add_argument("--gene_id", help = "ID of the gene to be extracted from the file.")
 args = parser.parse_args()
 
-print "CHR BP SNP P"
-command = " ".join(["zgrep", args.gene_id, args.fastqtl '| cut -f2,3,4,6 -d " "'])
+#print "CHR BP SNP P"
+command1 = 'printf "CHR BP SNP P\n"'
+command = " ".join(["zgrep", args.gene_id, args.fastqtl, '| cut -f2,3,4,6 -d " "'])
+subprocess.call(['bash','-c',command1])
 subprocess.call(['bash','-c',command])
+
