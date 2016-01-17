dca4439ad72c5a528c7a1e58b1d74376d26a2f5a
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-01-17 11:28:18 +0000

    Add flag to control memory usage

--- rasqual/bamCountASE.py
+++ rasqual/bamCountASE.py
@@ -12,6 +12,9 @@ parser.add_argument("--outsuffix", help = "Suffix of the output ASE count file."
 parser.add_argument("--reference", help = "Path to indexed reference FASTA file.")
 parser.add_argument("--sites", help = "Path to the VCF file containing SNP coordinates.")
 parser.add_argument("--execute", help = "Execute the script", default = "False")
+parser.add_argument("--Xmx", help = "Memory allocated to the Java process.", default = "900m")
+parser.add_argument("--java_path", help = "Path to the Java executable.", default = "/software/java/bin/java")
+parser.add_argument("--gatk_path", help = "Path to the GATK executable.", default = "~/software/GenomeAnalysisTK.jar")
 args = parser.parse_args()
 
 #Construct file names
@@ -19,7 +22,7 @@ for line in fileinput.input("-"):
 		sample_name = line.rstrip()
 		path_in = os.path.join(args.indir, sample_name, sample_name + args.insuffix)
 		path_out = os.path.join(args.outdir, sample_name, sample_name + args.outsuffix)
-		gatk_path = "/software/java/bin/java -jar -Xmx900m ~/software/GenomeAnalysisTK.jar -T ASEReadCounter"
+		gatk_path = " ".join([args.java_path, "-jar", "-Xmx"+args.Xmx, args.gatk_path, "-T ASEReadCounter"])
 		flags = "-U ALLOW_N_CIGAR_READS -dt NONE --minMappingQuality 10 -rf MateSameStrand"
 		command = " ".join([gatk_path, "-R", args.reference, "-I", path_in, "-o", path_out, "-sites", args.sites, flags])
 		print(command)
