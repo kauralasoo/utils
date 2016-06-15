4cf655160f28845bea66ef1ff486d7b9bcf29608
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-06-15 16:23:07 +0100

    Allow custom parameters to be passed onto featureCounts

--- bam/bam2counts.py
+++ bam/bam2counts.py
@@ -16,6 +16,7 @@ parser.add_argument("--unpaired", help = "BAM contains single-end reads.", defau
 parser.add_argument("--D", help = "Maximum insert size.", default = "2000")
 parser.add_argument("--donotsort", help = "Do not sort the BAM file", default = "False")
 parser.add_argument("--O", help = "Assign reads to all overlapping features.", default = "False")
+parser.add_argument("--parameters", help = "Additional parameters passed on to featureCounts. These must be in single quotes and the first dash must be escaped with the \\ character, for example '\-f'.")
 args = parser.parse_args()
 
 #Iterate over all ids
@@ -32,6 +33,10 @@ for line in fileinput.input("-"):
 		featureCounts_command = featureCounts_command + " -O"
 	if(args.unpaired == "False"):
 		featureCounts_command = " ".join([featureCounts_command, "-p -C -D", args.D, "-d 25"])
+	#Add any additional parameters to RASQUAL
+	if (args.parameters != None):
+		params = args.parameters[1:]#Remove the first escape character
+		featureCounts_command = " ".join([featureCounts_command, params])
 	command = " ".join([featureCounts_command, bam_file])
 	print(command)
 	if (args.execute == "True"):
