b56e6c3346dfc69403e11a73b096abdcfbc00257
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-11-28 20:46:59 +0000

    Exclude MHCII region from GARFIELD

--- GWAS/extractGarfieldColumns.py
+++ GWAS/extractGarfieldColumns.py
@@ -22,9 +22,22 @@ for line in fileinput.input("-"):
 	for i in range(1,23):
 		chr_string = "chr" + str(i)
 		output_file = os.path.join(output_directory, chr_string)
-		tabix_command = " ".join(["tabix", sorted_file, str(i), "| cut -f 3,6 --output-delimiter ' ' >", output_file])
-		print tabix_command
-		subprocess.call(['bash','-c',tabix_command])
+		if i == 6:
+			#Exclude the MHCII region
+			region_1 = "6:1-20000000"
+			tabix_command = " ".join(["tabix", sorted_file, str(i), "| cut -f 3,6 --output-delimiter ' ' >", output_file])
+			subprocess.call(['bash','-c',tabix_command])
+
+			region_2 = "6:40000000"
+			tabix_command = " ".join(["tabix", sorted_file, str(i), "| cut -f 3,6 --output-delimiter ' ' >>", output_file])
+			subprocess.call(['bash','-c',tabix_command])
+
+		else:
+			tabix_command = " ".join(["tabix", sorted_file, str(i), "| cut -f 3,6 --output-delimiter ' ' >", output_file])
+			print tabix_command
+			subprocess.call(['bash','-c',tabix_command])
+	
+	#create a seaprate header file
 	output_header_file = os.path.join(output_directory, "chrchr")
 	header_command = " ".join(["echo 'pos pval' >", output_header_file])
 	print(header_command)
