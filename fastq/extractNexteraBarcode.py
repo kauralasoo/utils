b6f1f14df6bf8bc3e42e0409183b8fb9b1a61553
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-01-02 16:18:13 +0200

    Fix elif

--- fastq/extractNexteraBarcode.py
+++ fastq/extractNexteraBarcode.py
@@ -14,7 +14,7 @@ if args.type == "read1":
 	command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTCCGAGCCCACGAGAC",
 		" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTCCGAGCCCACGAGAC//g'",
 		" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
-else if args.type == "read2":
+elif args.type == "read2":
 	command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTGACGCTGCCGACGA",
 		" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTGACGCTGCCGACGA//g'",
 		" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
