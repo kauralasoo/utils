589d5983086a7726678b83a18b6011882ae191e4
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-01-02 16:08:27 +0200

    print only the barcode and nothing else

--- fastq/extractNexteraBarcode.py
+++ fastq/extractNexteraBarcode.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 #Construct a command
 command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTCCGAGCCCACGAGAC",
 	" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTCCGAGCCCACGAGAC//g'",
-	" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1"])
+	" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
 
 #Run and report results to STDOUT
 subprocess.call(['bash','-c',command])
