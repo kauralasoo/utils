adeb35fee3108f3c43dd0b1a8c47dfb161100ee1
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-01-02 16:16:18 +0200

    Extract barcodes from either read1 or read2

--- fastq/extractNexteraBarcode.py
+++ fastq/extractNexteraBarcode.py
@@ -6,12 +6,20 @@ import subprocess
 
 parser = argparse.ArgumentParser(description = "Extract Nextera barcode from the fastq file.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--fastq", help = "Path to the fastq file.")
+parser.add_argument("--type", help = "Either read1 or read2.")
 args = parser.parse_args()
 
 #Construct a command
-command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTCCGAGCCCACGAGAC",
-	" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTCCGAGCCCACGAGAC//g'",
-	" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
+if args.type == "read1":
+	command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTCCGAGCCCACGAGAC",
+		" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTCCGAGCCCACGAGAC//g'",
+		" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
+else if args.type == "read2":
+	command = " ".join(["zcat ", args.fastq, " | grep CTGTCTCTTATACACATCTGACGCTGCCGACGA",
+		" | sed 's/$/XXXXXXXXXXXX/g;s/[ATGCNatgcn]*CTGTCTCTTATACACATCTGACGCTGCCGACGA//g'",
+		" | cut -c 1-8 | grep -v X | sort | uniq -c | sort -n | tail -n 1 | awk '{print $2}'"])
+else:
+	error("Unknown read type specified.")
 
 #Run and report results to STDOUT
 subprocess.call(['bash','-c',command])
